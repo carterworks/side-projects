ac6871452b65b37d22824ba7b7621f5173cb783b
Carter McBride <junk@carter.works>
2020-03-28 16:41:04 -0600

    reddit-client - Create default blue header

--- reddit-client-preact/src/components/header/index.js
+++ reddit-client-preact/src/components/header/index.js
@@ -1,21 +1,9 @@
 import { h } from "preact";
 import { Link } from "preact-router/match";
-import style from "./style.css";
 
 const Header = () => (
-  <header class={style.header}>
-    <h1>Preact App</h1>
-    <nav>
-      <Link activeClassName={style.active} href="/">
-        Home
-      </Link>
-      <Link activeClassName={style.active} href="/profile">
-        Me
-      </Link>
-      <Link activeClassName={style.active} href="/profile/john">
-        John
-      </Link>
-    </nav>
+  <header class="h-auto p-2 flex items-center bg-blue-500 font-sans text-gray-100">
+    <h1 class="text-3xl">Reddit Client—Preact</h1>
   </header>
 );
 
--- reddit-client-preact/src/routes/home/index.js
+++ reddit-client-preact/src/routes/home/index.js
@@ -1,8 +1,7 @@
 import { h } from "preact";
-import style from "./style";
 
 const Home = () => (
-  <div class={style.home}>
+  <div>
     <h1>Home</h1>
     <p>This is the Home component.</p>
 
